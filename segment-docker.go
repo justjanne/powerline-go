1aff8a6f5ba7c84adc02e28937554f7a480cee6e
Martin Englund <martin@englund.nu>
2017-10-04 20:47:35 -0700

    use DOCKER_HOST if present

--- segment-docker.go
+++ segment-docker.go
@@ -2,14 +2,27 @@ package main
 
 import (
 	"fmt"
+	"net/url"
 	"os"
 )
 
 func segmentDocker(p *powerline) {
+	var docker string
 	dockerMachineName, _ := os.LookupEnv("DOCKER_MACHINE_NAME")
+	dockerHost, _ := os.LookupEnv("DOCKER_HOST")
+
 	if dockerMachineName != "" {
+		docker = dockerMachineName
+	} else if dockerHost != " " {
+		u, err := url.Parse(dockerHost)
+		if err == nil {
+			docker = u.Host
+		}
+	}
+
+	if docker != "" {
 		p.appendSegment("docker", segment{
-			content:    fmt.Sprintf(" %s ", dockerMachineName),
+			content:    fmt.Sprintf(" %s ", docker),
 			foreground: p.theme.DockerMachineFg,
 			background: p.theme.DockerMachineBg,
 		})
