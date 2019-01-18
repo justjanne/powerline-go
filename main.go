5d93681fd7886f2eba26a3768bb1be92ec4e4e6e
Paul Graham <pgrahamdev@gmail.com>
2019-01-17 22:58:54 -0700

    main.go: Added "svn" as a valid choice in the usage string for the "priority" command-line option

--- main.go
+++ main.go
@@ -179,7 +179,7 @@ func main() {
 			"priority",
 			"root,cwd,user,host,ssh,perms,git-branch,git-status,hg,jobs,exit,cwd-path",
 			commentsWithDefaults("Segments sorted by priority, if not enough space exists, the least priorized segments are removed first. Separate with ','",
-				"(valid choices: aws, cwd, cwd-path, docker, exit, git-branch, git-status, hg, host, jobs, load, nix-shell, perlbrew, perms, root, ssh, time, user, venv, vgo)")),
+				"(valid choices: aws, cwd, cwd-path, docker, exit, git-branch, git-status, hg, host, jobs, load, nix-shell, perlbrew, perms, root, ssh, svn, time, user, venv, vgo)")),
 		MaxWidthPercentage: flag.Int(
 			"max-width",
 			0,
