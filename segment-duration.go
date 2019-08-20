justjanne/powerline-go

commit ff5bb75f085e9c222f00d2c7c6e83311bc2ff858

Fixes #108

segment-duration.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -37,7 +38,7 @@ func segmentDuration(p *powerline) {
 
 	hasPrecision := strings.Index(durationValue, ".") != -1
 
-	d, err := time.ParseDuration(durationValue + "s")
+	durationFloat, err := strconv.ParseFloat(durationValue, 64)
 	if err != nil {
 		p.appendSegment("duration", segment{
 			content:    fmt.Sprintf("Failed to convert '%s' to a number", *p.args.Duration),
@@ -47,9 +48,11 @@ func segmentDuration(p *powerline) {
 		return
 	}
 
-	if d > 0 {
+	duration := time.Duration(durationFloat) * time.Second
+
+	if duration > 0 {
 		var content string
-		ns := d.Nanoseconds()
+		ns := duration.Nanoseconds()
 		if ns > hours {
 			hrs := ns / hours
 			ns -= hrs * hours

commit ab1bfb1ed6bc2e2a56c4b7463ce9826d1f0dcee9

Fix regression introduced in ff5bb75f

segment-duration.go
@@ -48,7 +48,7 @@ func segmentDuration(p *powerline) {
 		return
 	}
 
-	duration := time.Duration(durationFloat) * time.Second
+	duration := time.Duration(durationFloat * float64(time.Second.Nanoseconds()))
 
 	if duration > 0 {
 		var content string