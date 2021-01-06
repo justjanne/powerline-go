b9311ce030963ddb0ede027ddadad238512017c9
Janne Koschinski <janne@kuschku.de>
2021-01-06 15:30:44 +0100

    [#265] Fix issue with exitcode segment not compiling on windows

--- segment-exitcode.go
+++ segment-exitcode.go
@@ -2,86 +2,31 @@ package main
 
 import (
 	"fmt"
+	"github.com/justjanne/powerline-go/exitcode"
 	pwl "github.com/justjanne/powerline-go/powerline"
 	"strconv"
-	"syscall"
 )
 
+var exitCodes = map[int]string{
+	1:   "ERROR",
+	2:   "USAGE",
+	127: "NOTFOUND",
+}
+
 func getMeaningFromExitCode(exitCode int) string {
-	switch exitCode {
-	case 1:
-		return "ERROR"
-	case 2:
-		return "USAGE"
-	case 126:
-		return "NOPERM"
-	case 127:
-		return "NOTFOUND"
-	case 128 + int(syscall.SIGHUP):
-		return "SIGHUP"
-	case 128 + int(syscall.SIGINT):
-		return "SIGINT"
-	case 128 + int(syscall.SIGQUIT):
-		return "SIGQUIT"
-	case 128 + int(syscall.SIGILL):
-		return "SIGILL"
-	case 128 + int(syscall.SIGTRAP):
-		return "SIGTRAP"
-	case 128 + int(syscall.SIGABRT):
-		return "SIGABRT"
-	case 128 + int(syscall.SIGBUS):
-		return "SIGBUS"
-	case 128 + int(syscall.SIGFPE):
-		return "SIGFPE"
-	case 128 + int(syscall.SIGKILL):
-		return "SIGKILL"
-	case 128 + int(syscall.SIGUSR1):
-		return "SIGUSR1"
-	case 128 + int(syscall.SIGSEGV):
-		return "SIGSEGV"
-	case 128 + int(syscall.SIGUSR2):
-		return "SIGUSR2"
-	case 128 + int(syscall.SIGPIPE):
-		return "SIGPIPE"
-	case 128 + int(syscall.SIGALRM):
-		return "SIGALRM"
-	case 128 + int(syscall.SIGTERM):
-		return "SIGTERM"
-	case 128 + int(syscall.SIGSTKFLT):
-		return "SIGSTKFLT"
-	case 128 + int(syscall.SIGCHLD):
-		return "SIGCHLD"
-	case 128 + int(syscall.SIGCONT):
-		return "SIGCONT"
-	case 128 + int(syscall.SIGSTOP):
-		return "SIGSTOP"
-	case 128 + int(syscall.SIGTSTP):
-		return "SIGTSTP"
-	case 128 + int(syscall.SIGTTIN):
-		return "SIGTTIN"
-	case 128 + int(syscall.SIGTTOU):
-		return "SIGTTOU"
-	case 128 + int(syscall.SIGURG):
-		return "SIGURG"
-	case 128 + int(syscall.SIGXCPU):
-		return "SIGXCPU"
-	case 128 + int(syscall.SIGXFSZ):
-		return "SIGXFSZ"
-	case 128 + int(syscall.SIGVTALRM):
-		return "SIGVTALRM"
-	case 128 + int(syscall.SIGPROF):
-		return "SIGPROF"
-	case 128 + int(syscall.SIGWINCH):
-		return "SIGWINCH"
-	case 128 + int(syscall.SIGIO):
-		return "SIGIO"
-	case 128 + int(syscall.SIGPWR):
-		return "SIGPWR"
-	case 128 + int(syscall.SIGSYS):
-		return "SIGSYS"
-	default:
-		return fmt.Sprintf("%d", exitCode)
+	if exitCode < 128 {
+		name, ok := exitCodes[exitCode]
+		if ok {
+			return name
+		}
+	} else {
+		name, ok := exitcode.Signals[exitCode-128]
+		if ok {
+			return name
+		}
 	}
+
+	return fmt.Sprintf("%d", exitCode)
 }
 
 func segmentExitCode(p *powerline) []pwl.Segment {
