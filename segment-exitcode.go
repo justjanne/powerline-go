69d23cda408f153d0d2e26fde83249b48ff9a2d8
Ville Skyttä <ville.skytta@iki.fi>
2021-11-02 22:02:27 +0200

    Add more exit code strings

--- segment-exitcode.go
+++ segment-exitcode.go
@@ -9,9 +9,28 @@ import (
 )
 
 var exitCodes = map[int]string{
-	1:   "ERROR",
+	// 1: generic
+	1: "ERROR",
+	// 2, 126, 127: common shell conventions, e.g. bash, zsh
 	2:   "USAGE",
+	126: "NOEXEC",
 	127: "NOTFOUND",
+	// 64-78: BSD, sysexits.h
+	64: "USAGE",
+	65: "DATAERR",
+	66: "NOINPUT",
+	67: "NOUSER",
+	68: "NOHOST",
+	69: "UNAVAILABLE",
+	70: "SOFTWARE",
+	71: "OSERR",
+	72: "OSFILE",
+	73: "CANTCREAT",
+	74: "IOERR",
+	75: "TEMPFAIL",
+	76: "PROTOCOL",
+	77: "NOPERM",
+	78: "CONFIG",
 }
 
 func getMeaningFromExitCode(exitCode int) string {
